a47d9f628d9bfb0dc6a7d57ea67d4b3cbc9f3447
adithya_ov <111501017@smail.iitpkd.ac.in>
2017-11-06 03:32:59 +0530

    Almost made, neetd to test

--- app/webpy_new/server.py
+++ app/webpy_new/server.py
@@ -7,7 +7,7 @@ urls = (
 )
 
 app = web.application(urls, locals())
-session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={})
+session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'logged_in': False})
 
 def auth_user(user):
     session.logged_in = True
@@ -18,9 +18,13 @@ def get_current_user():
     if session.logged_in:
         return User.get(User.id == session.user_id)
 
-def login_required():
+def should_be_logged_in():
     if not session.logged_in:
         raise web.redirect('/login')
+
+def should_be_logged_out():
+    if session.logged_in:
+        raise web.redirect('/timeline')
 
 def get_object_or_throw(model, *expressions):
     try:
@@ -33,13 +37,23 @@ def is_following(from_user, to_user):
 
 class Timeline:
     def GET(self):
-        login_required()
+        should_be_logged_in()
         user = get_current_user()
         tweets = Tweet.select().where(Tweet.user << user.following())
         return render.timeline(tweets)
 
+    def POST(self):
+        should_be_logged_in()
+        user = get_current_user()
+        tweet = Tweet.create(
+            user=user,
+            content=request.form['content'],
+            pub_date=datetime.datetime.now())
+        raise web.redirect('/timeline')
+
 class Register:
     def POST(self):
+        should_be_logged_out()
         try:
             post_data = web.input(_method='post')
             with database.transaction():
@@ -52,10 +66,82 @@ class Register:
             auth_user(user)
             raise web.redirect('/timeline')
         except:
-            raise web.redirect('/error')
+            raise web.redirect('/register')
+
+    def GET(self):
+        should_be_logged_out()
+        return render.register()
 
 
+class Login:
+    def POST(self):
+        should_be_logged_out()
+        try:
+            post_data = web.input(_method='post')
+            user = User.get(
+                username=post_data['username'],
+                password=post_data['password'])
+        except:
+            raise web.redirect('/login')
+        else:
+            auth_user(user)
+            raise web.redirect('/timeline')
 
+    def GET(self):
+        should_be_logged_out()
+        return render.login()
+
+
+class Logout:
+    def GET(self):
+        should_be_logged_in()
+        session.logged_in = False
+        raise web.redirect('/home')
+
+class Following:
+    def GET(self):
+        should_be_logged_in()
+        user = get_current_user()
+        return render.following(user.following())
+
+class Followers:
+    def GET(self):
+        should_be_logged_in()
+        user = get_current_user()
+        return render.followers(user.followers())
+
+class Users:
+    def GET(self, username):
+        should_be_logged_in()
+        if username == None or username == '_':
+            users = User.select()
+            return render.users(users)
+        else:
+             user = get_object_or_throw(User, User.username == username)
+             tweets = Tweet.select().where(user=user).order_by(('pub_date', 'desc'))
+             return render.user_detail(user, tweets)
+
+class Follow:
+    def GET(self, username, action):
+        should_be_logged_in()
+        if action == 'follow':
+            user = get_object_or_throw(User, User.username == username)
+            try:
+                with database.transaction():
+                    Relationship.create(
+                        from_user=get_current_user(),
+                        to_user=user)
+            except:
+                pass
+            raise web.redirect('/following')
+
+        if action == 'unfollow':
+            user = get_object_or_throw(User, User.username == username)
+            Relationship.delete().where(
+                (Relationship.from_user == get_current_user()) &
+                (Relationship.to_user == user)
+            ).execute()
+            raise web.redirect('/following')
 
 
 if __name__ == "__main__":
